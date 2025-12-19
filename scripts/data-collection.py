107b98b969ba9a25eac5a9ccda5ef8e560ea719c
Aditya Singh <adityapsingh1815@gmail.com>
2025-12-19 15:28:40 -0600

    Added the rest of the functions into data collection

--- scripts/data-collection.py
+++ scripts/data-collection.py
@@ -31,3 +31,32 @@ def get_team_stats(season='2025-26'):
     team_stats_df = team_stats.get_data_frames()[0]
     print(f'Fetched {len(team_stats_df)} team stats records.')
     return team_stats_df
+
+def get_advanced_stats(season='2025-26'):
+    """Fetching advanced player stats for the given season"""
+    print(f'Fetching player stats for season {season}...')
+    advanced_stats = leaguedashplayerstats.LeagueDashPlayerStats(
+        season=season,
+        measure_type_detailed_defense='Advanced',
+        per_mode_detailed='PerGame',
+        season_type_all_star='Regular Season' 
+    )
+    advanced_stats_df = advanced_stats.get_data_frames()[0]
+    print(f'Fetched {len(advanced_stats_df)} player advanced stats records.')
+    return advanced_stats_df 
+
+def get_all_players_info():
+    """Fetching all players info"""
+    print('Fetching all players info...')
+    all_players = players.get_players()
+    all_players_df = pd.DataFrame(all_players)
+    print(f'Fetched info for {len(all_players_df)} players.')
+    return all_players_df
+
+def get_all_teams_info():
+    """Fetching all teams info"""
+    print('Fetching all teams info...')
+    all_teams = teams.get_teams()
+    all_teams_df = pd.DataFrame(all_teams)
+    print(f'Fetched info for {len(all_teams_df)} teams.')
+    return all_teams_df
